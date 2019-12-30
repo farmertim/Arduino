ddb035baab358ddff957f955558c72aa928fdedd
farmertim <52063135+farmertim@users.noreply.github.com>
2019-12-30 10:02:55 +0800

    增加蜂鳴器

---
+++ Arduinofinish/Arduinofinish.ino
@@ -0,0 +1,72 @@
+ int LED=13;
+int LED1=12;
+int LED2=6;
+int isobstaclePin=7;
+int isobstaclePin1=8;
+int isobstacle=HIGH;
+int isobstacle1=HIGH;
+int check=0,check1=0;
+int buzzerPin=9;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED,OUTPUT);
+pinMode(LED1,OUTPUT);
+pinMode(LED2,OUTPUT);
+pinMode(buzzerPin,OUTPUT);
+pinMode(isobstaclePin,INPUT);
+pinMode(isobstaclePin1,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+isobstacle=digitalRead(isobstaclePin);
+isobstacle1=digitalRead(isobstaclePin1);
+
+if(isobstacle1==LOW){
+  Serial.println("obstacle!!,obstacle");
+  digitalWrite(LED1,HIGH);
+  check=1;
+  //int x=digitalRead(isobstaclePin);
+  //Serial.println(x);
+  }else{
+    Serial.println("clear");
+    digitalWrite(LED1,LOW);
+     check=0;
+  }
+
+if(isobstacle==LOW){
+  Serial.println("obstacle!!,obstacle");
+  digitalWrite(LED,HIGH);
+  check1=1;
+  
+  }else{
+    Serial.println("clear");
+    digitalWrite(LED,LOW);
+    check1=0;
+  }
+  if(check==1&&check1==1){
+    digitalWrite(buzzerPin,HIGH);
+    for(int i=0;i<1800;i++){
+    tone(buzzerPin,i,10);
+    delay(1);
+    }
+  for(int i=1800;i>150;i--){
+    tone(buzzerPin,i,10);
+    delay(1);
+    }
+    
+    digitalWrite(buzzerPin,LOW);
+    digitalWrite(LED2,HIGH);
+     digitalWrite(LED,LOW);
+      digitalWrite(LED1,LOW);
+    
+    }
+    else{
+       digitalWrite(buzzerPin,LOW);
+       digitalWrite(LED2,LOW);
+      }
+  delay(200);
+}
